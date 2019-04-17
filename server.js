91ad44e9de4962241b9557fc7f2819b76f251ed3
Tilakpur <tilakpur@in-Tilakpur.local>
2019-04-17 17:03:22 +0530

    package

--- server.js
+++ server.js
@@ -24,5 +24,8 @@ if (isProduction) {
   app.get("*", (req, res) => {
     res.sendfile("/react-app/build/index.html");
   });
+  app.get("/", (req, res) => {
+    res.sendfile("/react-app/build/index.html");
+  });
 }
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
